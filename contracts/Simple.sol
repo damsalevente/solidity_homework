500f050aebcb5aaff485ecdb410507d427568410
nap <nap@pop-os.localdomain>
2021-03-04 17:24:01 +0100

    first tool project

---
+++ contracts/Simple.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Simple {
+    uint32 balance;
+
+    struct Tool {
+        uint32 price;
+        address owner;
+    }
+
+    mapping(string => Tool) tools;
+
+    constructor() {
+        balance = 100;
+    }
+
+    function askFor(string memory what) public {
+        require(tools[what].price <= balance);
+        balance -= tools[what].price;
+        tools[what].owner = msg.sender;
+    }
+
+    function getTool(string memory what) public view returns (uint32, address) {
+        return (tools[what].price, tools[what].owner);
+    }
+}
